b7699c7645b44807b01bb83a6e483a55bea5069e
22p31a4410 <119730504+22p31a4410@users.noreply.github.com>
2022-12-23 19:25:15 +0530

    #include<stdio.h> int main() {     float bs,gs,da,hra;     scanf("%f",&bs);     if(bs<=10000)     {         da=bs*80/100;         hra=bs*20/100;     }     else if(bs<=20000)     {         da=bs*90/100;         hra=bs*25/100;     }     else     {         da=bs*95/100;         hra=bs*30/100;     }     gs=bs+hra+da;     printf("%.2f",gs); }

---
+++ Gross_Salary.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    float bs,gs,da,hra;
+    scanf("%f",&bs);
+    if(bs<=10000)
+    {
+        da=bs*80/100;
+        hra=bs*20/100;
+    }
+    else if(bs<=20000)
+    {
+        da=bs*90/100;
+        hra=bs*25/100;
+    }
+    else
+    {
+        da=bs*95/100;
+        hra=bs*30/100;
+    }
+    gs=bs+hra+da;
+    printf("%.2f",gs);
+}
