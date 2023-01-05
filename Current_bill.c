acf2ccf56932ba03db70498032eb889ecc4d4d9d
22p31a4410 <119730504+22p31a4410@users.noreply.github.com>
2023-01-05 14:40:39 +0530

    #include<stdio.h> int main() {     int n,q=0;     scanf("%d",&n);     float bill,sr,tb;     if(n<200)     {         bill=n*1.20;     }     else if(n>=200&&n<400)     {         bill=n*1.50;     }     else if(n>400&&n<600)     {         bill=n*1.80;     }     else     {         bill=n*2.00;     }     if(n>=400)     {         sr=bill*0.15;         tb=bill+sr;         printf("%.2f",tb);     }     else     {         tb=bill+100;         printf("%.2f",tb);     }
    }

---
+++ Current_bill.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main()
+{
+    int n,q=0;
+    scanf("%d",&n);
+    float bill,sr,tb;
+    if(n<200)
+    {
+        bill=n*1.20;
+    }
+    else if(n>=200&&n<400)
+    {
+        bill=n*1.50;
+    }
+    else if(n>400&&n<600)
+    {
+        bill=n*1.80;
+    }
+    else 
+    {
+        bill=n*2.00;
+    }
+    if(n>=400)
+    {
+        sr=bill*0.15;
+        tb=bill+sr;
+        printf("%.2f",tb);
+    }
+    else
+    {
+        tb=bill+100;
+        printf("%.2f",tb);
+    }
+    
+}
